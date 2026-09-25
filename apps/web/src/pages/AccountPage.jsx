4acdf54e527dba6451ab1d1ebafbd9b9862ed965
unjuandice3333-star <juaneslaley12@gmail.com>
2026-09-25 16:44:13 -0500

    fix: reset stored customer session on init to ensure login card is rendered

--- apps/web/src/pages/AccountPage.jsx
+++ apps/web/src/pages/AccountPage.jsx
@@ -27,7 +27,7 @@ export const AccountPage = () => {
   useEffect(() => {
     try {
       const activeSession = sessionStorage.getItem('jm_customer_session');
-      if (!activeSession && isCustomerLoggedIn) {
+      if (!activeSession) {
         customerLogout();
       }
     } catch (e) {}
