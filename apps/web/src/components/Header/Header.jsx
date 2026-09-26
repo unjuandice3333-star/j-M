ca585cc6b16ce2ace7748db98e13970fc2e61e17
unjuandice3333-star <juaneslaley12@gmail.com>
2026-09-25 23:37:02 -0500

    refactor: completely remove account module, user icon, and login flow

--- apps/web/src/components/Header/Header.jsx
+++ apps/web/src/components/Header/Header.jsx
@@ -151,13 +151,8 @@ export const Header = () => {
               <Search size={21} />
             </button>
 
-            {/* Account Link */}
-            <Link to="/cuenta" style={iconBtnStyle} title="Mi cuenta">
-              <User size={21} />
-            </Link>
-
             {/* Wishlist Link */}
-            <Link to="/cuenta/favoritos" style={{ ...iconBtnStyle, position: 'relative' }} title="Favoritos">
+            <Link to="/favoritos" style={{ ...iconBtnStyle, position: 'relative' }} title="Favoritos">
               <Heart size={21} />
               {wishlist.length > 0 && (
                 <span style={badgeStyle}>{wishlist.length}</span>
